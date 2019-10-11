7aa19976780a739d4fead7f5a34c50df9acdc170
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-11 14:37:07 +0100

    Unicorn in the making

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,63 @@
+require 'pry'
+
 class VentureCapitalist
 
+    attr_reader :name
+    attr_accessor :total_worth, :funding_rounds
+
+    @@all = []
+
+    def initialize(name, total_worth)
+
+        @name = name
+        @total_worth = total_worth
+        
+
+        VentureCapitalist.all << self
+        
+    end
+
+    def self.all
+
+        @@all
+
+    end
+
+    def self.tres_commas_club
+
+        VentureCapitalist.all.select {|vc| vc.total_worth > 1000000000}
+        
+    end
+    
+    def offer_contract(startup, type, amount)
+
+        FundingRound.new(startup, self, type, amount)
+        
+    end
+
+    def funding_rounds
+
+        FundingRound.all.select {|round| round.vc == self}
+        
+    end
+
+    def portfolio
+
+        self.funding_rounds.map {|round| round.startup}.uniq
+        
+    end
+
+    def biggest_investment
+
+        self.funding_rounds.max_by {|round| round.investment}
+        
+    end
+
+    def invested(domain)
+
+        var1 = self.funding_rounds.select {|round| round.startup.domain.include?(domain)}
+        var1.map {|round| round.investment}.sum
+
+    end
+    
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,21 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+company1 = Startup.new("Kupp", "John Adams", "Blockchain plastic cups")
+company2 = Startup.new("Chaarg", "Nick T.", "Charging appliances through MLMs")
+company3 = Startup.new("Bocks", "N. Nedelchev", "Distrupt Big Cardboard")
+
+vc1 = VentureCapitalist.new("William G.", 12000000000)
+vc2 = VentureCapitalist.new("Jeffrey B.", 123000000000)
+vc3 = VentureCapitalist.new("Kevin O.", 400000000)
+
+company1.sign_contract(vc1, "Angel", 12)
+company1.sign_contract(vc2, "Series-A", 100000)
+company2.sign_contract(vc3, "Series-B", 7000000)
+company3.sign_contract(vc1, "Angel", 7)
+
+round1 = FundingRound.new(company1, vc1, "Angel", 100000000)
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
