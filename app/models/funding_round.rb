e3db277a41d306994f37a35678bf25236c96f736
Liko <li_xie1992@hotmail.com>
2019-10-11 15:30:32 +0100

    done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,26 @@
 class FundingRound
 
+    attr_reader :startup, :venture_capitalist, :funding_round_type, :investment_amount
+
+    @@all = []
+
+    def initialize(startup, venture_capitalist, funding_round_type, investment_amount)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @funding_round_type = funding_round_type
+        @investment_amount = investment_amount.to_f
+        @@all << self
+    end 
+
+    def investment_amount=(amount)
+        if amount < 0
+            @investment_amount = 0.0
+        end
+        @investment_amount = amount.to_f
+    end 
+
+    def self.all
+        @@all
+    end 
+
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,56 @@
 class Startup
+    
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(new_domain, new_name)
+        self.domain = new_domain
+        self.name = new_name
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.find_by_founder(founder_name)
+        self.all.find{ |startup| startup.founder == founder_name }
+    end
+
+    def self.domains
+        self.all.map{|startup| startup.domain }.uniq
+    end
+
+    def sign_contract(venture_capitalist, funding_round_type, investment_amount)
+        FundingRound.new(self, venture_capitalist, funding_round_type, investment_amount)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.count{ |round| round.startup == self }
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|round| round.startup == self}
+    end
+
+    def total_funds
+        self.funding_rounds.map{|round| round.investment_amount}.sum
+    end
+
+    def investors
+        self.funding_rounds.map{ |round| round.venture_capitalist }.uniq
+    end 
+
+    def big_investors
+        self.investors.select{ |vc| vc.total_worth > 1000000000 }
+    end 
 
 end
