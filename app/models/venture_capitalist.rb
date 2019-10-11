086539a45cd86da832d92756f962ade90b58a2aa
cseiasp <caroline.seilern@hotmail.com>
2019-10-11 16:47:26 +0100

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,49 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth.to_i
+        VentureCapitalist.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tres_commas?
+        @total_worth > 1000000000
+    end
+
+    def self.tres_commas_club
+        instance = VentureCapitalist.all.select {|vc| vc.total_worth > 1000000000}
+        instance.map {|vc| vc.name}
+    end
+
+    def offer_contract(startup, type, amount)
+        FundingRound.new(startup, self, type, amount)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|round| round.vc == self}
+    end
+
+    def portfolio
+        funding_rounds.map {|round| round.startup}
+    end
+
+    def biggest_investment
+        funding_rounds.max_by {|round| round.amount}
+    end
+
+    def invested(domain)
+        funding_rounds.reduce(0) do |sum, round|  
+            if round.startup.domain == domain
+                sum += round.amount
+            end 
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,20 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+startup1 = Startup.new("star - tup - uan", "founder", "@domain.com")
+startup2 = Startup.new("star - tup - uan", "founder", "@domain.com")
+startup3 = Startup.new("star - tup - uan", "founder", "@domain.com")
+vc2 = VentureCapitalist.new("vc1", 100000000000000000)
+vc3 = VentureCapitalist.new("vc2", 100000000000000000)
+vc4 = VentureCapitalist.new("vc3", 1000000)
+funding1 = FundingRound.new("round1", "vc", "type", -1)
+
+test = startup1.sign_contract("vc1", "type", 10)
+test2 = startup1.sign_contract(vc2, "type", 10000)
+test3 = startup2.sign_contract(vc2, "type", 100)
+test2 = startup3.sign_contract(vc2, "type", 10)
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
