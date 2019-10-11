7d9907eb29a4542ebe80e63c6b793b83f8e39bee
gortron <gordon.lanza@gmail.com>
2019-10-11 14:17:15 +0100

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class VentureCapitalist
+  attr_accessor :total_worth
+  attr_reader :name
+  @@all = []
 
+  def initialize(name, total_worth)
+    @name = name
+    # does total_worth have arbitrary amount?
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    self.all.select { |vc| vc.total_worth > 1000000000 }
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+    @total_worth -= investment
+  end
+
+  def funding_rounds
+    FundingRound.all.select { |round| round.venture_capitalist == self }
+  end
+
+  def portfolio
+    self.funding_rounds.map { |round| round.startup }
+  end
+
+  def biggest_investment
+    #self.funding_rounds.map { |round| round.investment }.sort.max 
+    self.funding_rounds.sort_by(&:investment).last
+  end
+
+  def self.invested(domain)
+    #self.portfolio.select { |startup| startup.domain == domain } 
+    #then grab @investment and .sum it
+
+    FundingRound.all.select { |round| round.startup.domain == domain }.map { |round| round.investment}.sum
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,33 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+vc1 = VentureCapitalist.new("alex", 10000000000)
+vc2 = VentureCapitalist.new("gordy", 999999999)
+vc3 = VentureCapitalist.new("peter", 999999998)
+su1 = Startup.new("rugby heaven","shomz", "sports")
+su2 = Startup.new("i love maths", "lanza", "maths")
+su3 = Startup.new("peter's playhouse", "ayeni", "engineering")
+vc1.offer_contract(su1, "seed", 50000)
+vc1.offer_contract(su1, "series-a", 5000000)
+vc2.offer_contract(su1, "series-a-bridge", 250000)
+vc2.offer_contract(su2, "series-a", 2000000)
+su3.sign_contract(vc3, "seed", 200000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+#VC
+# Tres Commas x
+# offer_contract x
+# funding_rounds x
+# portfolio x
+# biggest_investment x
+# invested x
+
+#Startup
+# sign contract x
+# num funding rounds x
+# total funds x 
+# investors x
+# big investors x
+
